fbd9d00c7f7b9df0f8b424f89e9a1ae3c08fc7bf
virdgin <am.klepik@yandex.ru>
2022-07-31 20:21:26 +0300

    31.07.2022 wayuo CSS generator js

---
+++ wayup/CSS Generator/main.js
@@ -0,0 +1,37 @@
+/* ИНПУТЫ*/
+const rangeTl = document.getElementById('tlr'),
+    rangeTr = document.getElementById('trr'),
+    rangeBl = document.getElementById('blr'),
+    rangeBr = document.getElementById('brr');
+
+/*РЕЗУЛЬТАТЫ ИНПУТОВ*/
+const resultTl = document.getElementById('result-tlr'),
+    resultTr = document.getElementById('result-trr'),
+    resultBl = document.getElementById('result-blr'),
+    resultBr = document.getElementById('result-brr');
+
+const input = document.querySelectorAll('.input');
+const cube = document.getElementById('cube');
+
+const total = document.getElementById('totalResult');
+const btn = document.getElementById('btn');
+
+
+const changeRadius = () => {
+    resultTl.innerHTML = rangeTl.value;
+    resultTr.innerHTML = rangeTr.value;
+    resultBl.innerHTML = rangeBl.value;
+    resultBr.innerHTML = rangeBr.value;
+
+    cube.style.borderRadius = rangeTl.value + 'px ' + rangeTr.value + 'px ' + rangeBr.value + 'px ' + rangeBl.value + 'px';
+}
+
+const totalResult=() => {
+    total.innerHTML = rangeTl.value + ' ' + rangeTr.value + ' ' + rangeBr.value + ' ' + rangeBl.value + ' ';
+}
+
+for (node of input) {
+    node.addEventListener('input', changeRadius);
+}
+
+btn.addEventListener('click', totalResult);
